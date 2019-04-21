35a748f84b5888fffb2f8c929bdf31b198c04f0c
conanite <conan@conandalton.net>
2019-04-21 17:01:46 +0200

    subtract: allow missing #exlude_dates and function exactly like #include_dates; likewise allow missing #include_dates and function as inverse of #exclude_dates

--- lib/kalendor/subtract.rb
+++ lib/kalendor/subtract.rb
@@ -1,11 +1,11 @@
 module Kalendor
   module Subtract
     include DateHelper, Instance
-    def get_dates from, upto ;  _included_dates(from, upto) - _excluded_dates(from, upto) ; end
+    def get_dates from, upto ; in_dates(include_dates, from, upto) - ex_dates(exclude_dates, from, upto) ; end
 
     private
 
-    def _excluded_dates from, upto ; Set.new(exclude_dates.get_dates(from, upto)) ; end
-    def _included_dates from, upto ; Set.new(include_dates.get_dates(from, upto)) ; end
+    def in_dates cal, from, upto ; Set.new(cal ? cal.get_dates(from, upto) : (from..upto)) ; end
+    def ex_dates cal, from, upto ; Set.new(cal ? cal.get_dates(from, upto) : [])           ; end
   end
 end
--- spec/subtract_spec.rb
+++ spec/subtract_spec.rb
@@ -23,4 +23,26 @@ RSpec.describe Kalendor::Subtract do
 
     expect(summer_workdays.get_dates(date("1999-01-01"), date("2020-12-31")).to_a).to eq workdays_in_summer_2016
   end
+
+  it "ignores nil exclude_dates" do
+    all_summer_long = Kalendor.build { subtract(interval("2016-06-01", "2016-08-31"), nil) }
+
+    days_in_jun_2016    = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30   ].map { |d| date("2016-06-#{d}") }
+    days_in_jul_2016    = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31].map { |d| date("2016-07-#{d}") }
+    days_in_aug_2016    = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31].map { |d| date("2016-08-#{d}") }
+    days_in_summer_2016 = days_in_jun_2016 + days_in_jul_2016 + days_in_aug_2016
+
+    expect(all_summer_long.get_dates(date("1999-01-01"), date("2020-12-31")).to_a).to eq days_in_summer_2016
+  end
+
+  it "ignores nil include_dates" do
+    no_weekends = Kalendor.build { subtract(nil, union(weekday(6), weekday(0))) }
+
+    weekdays_in_jun_2016    = [1,2,3,    6,7,8,9,10,      13,14,15,16,17,      20,21,22,23,24,      27,28,29,30   ].map { |d| date("2016-06-#{d}") }
+    weekdays_in_jul_2016    = [1,    4,5,6,7,8,     11,12,13,14,15,      18,19,20,21,22,      25,26,27,28,29      ].map { |d| date("2016-07-#{d}") }
+    weekdays_in_aug_2016    = [1,2,3,4,5,    8,9,10,11,12,      15,16,17,18,19,      22,23,24,25,26,      29,30,31].map { |d| date("2016-08-#{d}") }
+    weekdays_in_summer_2016 = weekdays_in_jun_2016 + weekdays_in_jul_2016 + weekdays_in_aug_2016
+
+    expect(no_weekends.get_dates(date("2016-06-01"), date("2016-08-31")).to_a).to eq weekdays_in_summer_2016
+  end
 end
